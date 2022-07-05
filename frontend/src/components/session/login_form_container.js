2965662098fbc521890e4dd755967753391a3c6b
App Academy Student <email@email.com>
2022-07-05 17:48:28 -0400

    moved most of frontend into src but not working yet

---
+++ frontend/src/components/session/login_form_container.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+import { login } from '../../actions/session_actions';
+import LoginForm from './login_form';
+
+const mapStateToProps = state => ({
+    currentUser: state.session.user,
+    errors: state.errors.session
+});
+
+const mapDispatchToProps = dispatch => ({
+    login: user => dispatch(login(user))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LoginForm);
