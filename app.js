ba9808a88dcc38b7a45f634f994125e042e88db9
Duncan Murchison <duncancmurchison@gmail.com>
2022-07-07 17:54:39 -0400

    moved if to top

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ const madlibs = require("./routes/api/madlibs")
 const User = require("./models/User");
 const Madlib = require("./models/Madlib")
 
+if (process.env.NODE_ENV === 'production') {
+  app.use("/", express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -30,12 +36,6 @@ app.get("/", (req, res) => {
   res.send("Welcome to MadLibs Against Humanity!");
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use("/", express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
-}
 
 app.use("/api/users", users);
 app.use("/api/madlibs", madlibs);
