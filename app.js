a732813b55252d3a14b12f3edc340f4e487bca1e
Duncan Murchison <duncancmurchison@gmail.com>
2022-07-07 22:15:33 -0400

    heroku 11

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 const express = require("express");
+const app = express();
 const mongoose = require('mongoose');
-const User = require("./models/User");
-const Madlib = require("./models/Madlib")
+const db = require('./config/keys').mongoURI;
+const bodyParser = require("body-parser");
+const path = require('path');
+
+// routes
 const users = require("./routes/api/users");
 const madlibs = require("./routes/api/madlibs")
-const bodyParser = require("body-parser");
-const passport = require('passport');
 
-const app = express();
+// models
+const User = require("./models/User");
+const Madlib = require("./models/Madlib")
 
-// Mongo DB
-const db = require('./config/keys').mongoURI;
+
 mongoose
 .connect(db, { useNewUrlParser: true })
 .then(() => console.log("Connected to MongoDB successfully"))
 .catch(err => console.log(err));
 
-const path = require('path');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const passport = require('passport');
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
 if (process.env.NODE_ENV === 'production') {
   app.use("/", express.static('frontend/build'));
   app.get('/', (req, res) => {
@@ -24,20 +34,14 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(passport.initialize());
-require('./config/passport')(passport);
-
 app.get("/", (req, res) => {
   res.send("React Broke!");
 });
 
-// Routes
+
 app.use("/api/users", users);
 app.use("/api/madlibs", madlibs);
 
-// Server
 const port = process.env.PORT || 5001;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
