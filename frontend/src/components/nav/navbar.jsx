69a221f791c6825c20980fb88714d8049e4e0a28
Duncan Murchison <duncancmurchison@gmail.com>
2022-07-13 12:38:14 -0400

    fixed navbar

--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import { withRouter } from 'react-router-dom';
-import '../../stylesheets/navbar.scss'
+import '../../stylesheets/navbar.scss';
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -11,66 +10,49 @@ class NavBar extends React.Component {
   }
 
   logoutUser(e) {
-      e.preventDefault();
-      this.props.logout();
-      window.location.reload(false);
-      this.props.history.push('/');
+    e.preventDefault();
+    this.props.logout();
+    window.location.reload(false);
+    this.props.history.push('/');
   }
 
   // Selectively render links dependent on whether the user is logged in
   getLinks() {
-      if (this.props.loggedIn) {
-        return (
-          <>
-            <div className='navbar-links'>
-              <Link className='users-madlibs' to={`/users/${this.props.currentUser.id}/madlibs`}>{this.props.currentUser.handle}'s' Madlibs</Link>
-              <Link className='create-madlib' to={'/madlibs/new'}>Create a Madlib</Link>
-            </div>
-            <button className='logout' onClick={this.logoutUser}>Logout</button>
-          </>
-        );
-      } else {
-        return (
+    if (this.props.loggedIn) {
+      return (
+        <>
           <div className='navbar-links'>
-            <Link className='signup' to={'/signup'}>Signup</Link>
-            <Link className='login' to={'/login'}>Login</Link>
+            <Link className='users-madlibs' to={`/users/${this.props.currentUser.id}/madlibs`}>{this.props.currentUser.handle}'s' Madlibs</Link>
+            <Link className='create-madlib' to={'/madlibs/new'}>Create a Madlib</Link>
           </div>
-        );
-      }
+          <button className='logout' onClick={this.logoutUser}>Logout</button>
+        </>
+      );
+    } else {
+      return (
+        <div className='navbar-links'>
+          <Link className='signup' to={'/signup'}>Signup</Link>
+          <Link className='login' to={'/login'}>Login</Link>
+        </div>
+      );
+    }
   }
 
   render() {
-      return (
-        <div className='header-container'>
-          <div className='header'>
-            <h1 className='header-h1'>
-              <Link className='header-h1-link' to={'/'}>Mad Liber</Link>
-            </h1>
-            <br />
-            <p>A website for Madlibs and Friends!</p>
-          </div>
-          <div className='navbar-container'>
-            {this.getLinks()}
-          </div>
+    return (
+      <div className='header-container'>
+        <div className='header'>
+          <h1 className='header-h1'>
+            <Link className='header-h1-link' to={'/'}>Mad Liber</Link>
+          </h1>
+          <br />
+          <p>A website for Madlibs and Friends!</p>
         </div>
-
-
-
-
-
-
-
-
-
-        // <div className='navbar-container'>
-        //   <header className='navbar-header'>
-        //     <h1>
-        //       <Link className='navbar-text' to='/'>MadLibs</Link>
-        //     </h1>
-        //     <span className='navbar-buttons'>{ this.getLinks() }</span>
-        //   </header>
-        // </div>
-      );
+        <div className='navbar-container'>
+          {this.getLinks()}
+        </div>
+      </div>
+    );
   }
 }
 
