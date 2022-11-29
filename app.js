0c678b4c67b254b21eeb2f07c50853e07b000152
dmurchison <duncancmurchison@gmail.com>
2022-11-28 20:04:52 -0500

    cors

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 const bodyParser = require("body-parser");
+const passport = require("passport");
 const path = require("path");
+const cors = require("cors");
 
 // routes
 const users = require("./routes/api/users");
@@ -15,24 +17,27 @@ const Madlib = require("./models/Madlib")
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
+  app.use(cors());
   
   app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   })
 }
 
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB"));
+
+// CORS
+app.use(cors());
   
-  
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-const passport = require("passport");
-app.use(passport.initialize());
-require("./config/passport")(passport);
 
 
 app.get("/", (req, res) => {
